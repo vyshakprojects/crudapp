4308044abdb73121c3fd9a10080c0adf461e08fd
vyshakprojects <vyshakm117@gmail.com>
2025-02-03 17:19:05 +0530

    corecting port number

--- app.py
+++ app.py
@@ -74,9 +74,5 @@ def delete_item(id):
 def health_check():
     return jsonify({'status': 'OK'}), 200
 
-@app.route('/health2', methods=['GET'])
-def health_check():
-    return jsonify({'status': 'OK'}), 200
-
 if __name__ == '__main__':
-    app.run(port=8082)
+    app.run(port=8080)
